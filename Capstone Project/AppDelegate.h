0779224957809dc018687e9b1fcabefd71e0a735
HugoGoHe <88735772+HugoGoHe@users.noreply.github.com>
2022-07-07 16:01:25 -0700

    Add files via upload

---
+++ Capstone Project/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  Capstone Project
+//
+//  Created by Hugo Gomez Herrera on 7/7/22.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Capstone Project/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  Capstone Project
+//
+//  Created by Hugo Gomez Herrera on 7/7/22.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
