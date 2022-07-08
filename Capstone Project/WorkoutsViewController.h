702f8776de39dfaf6c0a1e3aac920e8fdd223eeb
Hugo Alejandro Gomez Herrera <a01640856@itesm.mx>
2022-07-08 16:39:14 -0700

    Logout
    - Logout Button works

---
+++ Capstone Project/WorkoutsViewController.h
@@ -0,0 +1,16 @@
+//
+//  WorkoutsViewController.h
+//  Capstone Project
+//
+//  Created by Hugo Gomez Herrera on 7/8/22.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface WorkoutsViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
