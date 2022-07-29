07bcf93593ac91b95beb16611deab9a8dfda03b5
Hugo Alejandro Gomez Herrera <a01640856@itesm.mx>
2022-07-28 21:36:17 -0700

    [Post Preview page][Input validation]
    - If user does not enter a valid weight an error label is displayed and the keyboard is dismissed.

--- Capstone Project/ViewControllers/WeightLoss/PostPreviewViewController.m
+++ Capstone Project/ViewControllers/WeightLoss/PostPreviewViewController.m
@@ -11,6 +11,7 @@
 @interface PostPreviewViewController ()
 
 @property(nonatomic, strong) NSDate *currentDate;
+@property (weak, nonatomic) IBOutlet UILabel *errorLabel;
 
 @end
 
@@ -27,17 +28,26 @@
 }
 
 - (IBAction)didTapCheckMark:(id)sender {
-    [ProgressPic postUserImage:self.selectedImage withWeight:[self.weightLabel.text floatValue] withDate:self.currentDate withCompletion:^(BOOL succeeded, NSError * _Nullable error){
-        if(!error){
-            [self.delegate didPost];
-        }
-    }];
     
-    UIStoryboard *storyboard = [UIStoryboard storyboardWithName:@"Main" bundle:nil];
-    UITabBarController *nav = [storyboard instantiateViewControllerWithIdentifier: @"TabBarViewController"];
-    [nav setModalPresentationStyle:UIModalPresentationFullScreen];
-    [nav setSelectedViewController:[nav.viewControllers objectAtIndex:0]];
-    [self.navigationController presentViewController:nav animated:YES completion:nil];
+    if (!([self.weightLabel.text intValue] > 0 && [self.weightLabel.text intValue] < 1000)){
+        self.errorLabel.text = @"Type a valid weight";
+        //Hide the keyboard
+        [self.view endEditing:YES];
+    }else{
+        self.errorLabel.text = @"";
+        [ProgressPic postUserImage:self.selectedImage withWeight:[self.weightLabel.text floatValue] withDate:self.currentDate withCompletion:^(BOOL succeeded, NSError * _Nullable error){
+            if(!error){
+                [self.delegate didPost];
+            }
+        }];
+        
+        UIStoryboard *storyboard = [UIStoryboard storyboardWithName:@"Main" bundle:nil];
+        UITabBarController *nav = [storyboard instantiateViewControllerWithIdentifier: @"TabBarViewController"];
+        [nav setModalPresentationStyle:UIModalPresentationFullScreen];
+        [nav setSelectedViewController:[nav.viewControllers objectAtIndex:0]];
+        [self.navigationController presentViewController:nav animated:YES completion:nil];
+    }
+   
     
 }
 
